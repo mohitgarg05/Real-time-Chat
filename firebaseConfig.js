f9c390a6e8c5028558b9fea07b3977ff94ef4124
mohitgarg05 <83368553+mohitgarg05@users.noreply.github.com>
2023-01-16 22:02:35 +0530

    first commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,7 +6,7 @@ import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDDXvPPyCJ8JUVV4BgPOXpw3sQWFjIH7Bk",
-    authDomain: "https://real-time-chat-alpha.vercel.app/",
+    authDomain: "real-time-chat-10c5d.firebaseapp.com",
     projectId: "real-time-chat-10c5d",
     storageBucket: "real-time-chat-10c5d.appspot.com",
     messagingSenderId: "534298369372",
